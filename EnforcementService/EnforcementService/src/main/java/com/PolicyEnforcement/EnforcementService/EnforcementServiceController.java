6fe0ba1d5fc5da14a687f49407e90314fa7e4429
Ronan Byrne <ronan.byrne1@ucdconnect.ie>
2022-05-01 13:21:39 +0100

    Adding dependency on MongService to EnforcementService

---
+++ EnforcementService/EnforcementService/src/main/java/com/PolicyEnforcement/EnforcementService/EnforcementServiceController.java
@@ -0,0 +1,10 @@
+package com.PolicyEnforcement.EnforcementService;
+import com.PolicyEnforcement.MongoService.models.Policy;
+
+public class EnforcementServiceController {
+    
+    void testPolicyImport(){
+        Policy policy = new Policy();
+        
+    }
+}
